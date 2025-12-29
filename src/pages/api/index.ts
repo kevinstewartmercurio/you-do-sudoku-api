0e9220b3ebbcc6bd588094ff16bf6a9824529336
Kevin Stewart-Mercurio <kevinstewartmercurio@gmail.com>
2025-12-29 12:54:52 -0500

    fix: adding empty json response

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -88,7 +88,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  return res.status(500);
+  return res.status(500).json({});
 
   try {
     await client.connect();
